9517af48cad204a5377474ea23a540c1914ba8f1
luoyuzhyi@youzan.com <luoyuzhi@youzan.com>
2023-02-01 22:49:27 +0800

    liship

--- vendor/smalls/video-tools/src/Logic/LiVideoLogic.php
+++ vendor/smalls/video-tools/src/Logic/LiVideoLogic.php
@@ -22,19 +22,54 @@ class LiVideoLogic extends Base
 
     public function setContents()
     {
-        $contents       = $this->get($this->url, [], [
-            'User-Agent' => UserGentType::WIN_USER_AGENT,
-        ]);
-        $this->contents = $contents;
+        $contents = $this->pear($this->url);
+        $this->contents = $contents['data'];
+    }
+
+    function pear($url) {
+        $html = $this->curl($url);
+        preg_match('/<h1 class=\"video-tt\">(.*?)<\/h1>/', $html, $title);
+        preg_match('/_(\d+)/', $url, $feed_id);
+        $base_url = sprintf("https://www.pearvideo.com/videoStatus.jsp?contId=%s&mrd=%s", $feed_id[1], time());
+        $response = $this->pear_curl($base_url, $url);
+        $content = json_decode($response, true);
+        if ($content['resultCode'] == 1) {
+            $video = $content["videoInfo"]["videos"]["srcUrl"];
+            $cover = $content["videoInfo"]["video_image"];
+            $timer = $content["systemTime"];
+            $video_url = str_replace($timer, "cont-" . $feed_id[1], $video);
+            $arr = [
+                'code' => 200,
+                'msg' => '解析成功',
+                'data' => [
+                    'title' => $title[1],
+                    'cover' => $cover,
+                    'url' => $video_url,
+                    'time' => $timer,
+                ]
+            ];
+            return $arr;
+        }
+    }
+
+    private function pear_curl($url, $referer) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_REFERER, $referer);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip,deflate');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
     }
 
     public function setVideoUrl()
     {
-        preg_match('/videoCenterId:"(.*?)",/ix', $this->contents, $videoMatches);
-        if (CommonUtil::checkEmptyMatch($videoMatches)) {
-            throw new ErrorVideoException("视频URL获取失败");
-        }
-        $this->VideoUrl = $videoMatches[1];
+
     }
 
     /**
@@ -57,18 +92,33 @@ class LiVideoLogic extends Base
 
     public function getVideoDesc()
     {
-        return '';
+        return $this->contents['title'] == null ? '' : $this->contents['title'];
     }
 
     public function getVideoImage()
     {
-        return '';
+        return $this->contents['cover'] == null ? '' : $this->contents['cover'];
     }
 
     public function getVideoUrl()
     {
-        return $this->VideoUrl;
+        return $this->contents['url'] == null ? '' : $this->contents['url'];
     }
 
+    private function curl($url, $headers = []) {
+        $header = ['User-Agent:Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'];
+        $con = curl_init((string)$url);
+        curl_setopt($con, CURLOPT_HEADER, false);
+        curl_setopt($con, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($con, CURLOPT_RETURNTRANSFER, true);
+        if (!empty($headers)) {
+            curl_setopt($con, CURLOPT_HTTPHEADER, $headers);
+        } else {
+            curl_setopt($con, CURLOPT_HTTPHEADER, $header);
+        }
+        curl_setopt($con, CURLOPT_TIMEOUT, 10000);
+        $result = curl_exec($con);
+        return $result;
+    }
 
 }
